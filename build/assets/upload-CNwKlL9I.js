cb9b2abd05491cbeb3126e945b42cbb5a1dd2ef4
r4mxae <120150977+r4mxae@users.noreply.github.com>
2025-12-03 10:43:10 +0000

    add

---
+++ build/assets/upload-CNwKlL9I.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-DKFEh9ub.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16",key:"jecpp"}],["rect",{width:"20",height:"14",x:"2",y:"6",rx:"2",key:"i6l2r4"}]],c=e("briefcase",o);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["polyline",{points:"17 8 12 3 7 8",key:"t8dd8p"}],["line",{x1:"12",x2:"12",y1:"3",y2:"15",key:"widbto"}]],i=e("upload",t);export{c as B,i as U};
